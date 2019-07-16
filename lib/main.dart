72b90b0a672a32e5ad76a96b7ffa43272feeefcc
Muh. Rahmatullah <muhammadrahmatullah1@gmail.com>
2019-07-16 10:10:03 +0700

    remove unused resource

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'dart:developer';
-
-import 'package:favmovie/network/api.dart';
 import 'package:favmovie/ui/movie_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -16,67 +13,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blueGrey,
       ),
       home: MovieScreen(title: 'Fav Movie'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-
-  @override
-  void initState() {
-    super.initState();
-    _fetchMovieData();
-  }
-
-  Future<void> _fetchMovieData() async{
-
-    final api = Api();
-    final jsonResult = await api.fetchMovieList('popular');
-    print(jsonResult);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
     );
   }
 }
